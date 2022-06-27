39d3bcb08a55d6b53d96ffd661806cd490242942
Jose <jose.valladares@mail.udp.cl>
2022-06-27 15:51:47 -0400

    Fix WebView
    Error when opening an internal twitter website

--- app/src/main/java/com/sample/desafio/presentation/details/DetailsActivity.kt
+++ app/src/main/java/com/sample/desafio/presentation/details/DetailsActivity.kt
@@ -4,18 +4,36 @@ import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import android.webkit.WebView
+import android.webkit.WebViewClient
 import androidx.appcompat.app.AppCompatActivity
 import com.sample.desafio.R
+import com.sample.desafio.databinding.ActivityDetailsBinding
 import com.sample.desafio.presentation.hits.HitStateUi
 
 class DetailsActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityDetailsBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_details)
+        binding = ActivityDetailsBinding.inflate(layoutInflater)
+        setContentView(binding.root)
         val hit = intent.getParcelableExtra<HitStateUi>(HIT_EXTRA)
         val myWebView: WebView = findViewById(R.id.wvContent)
-        myWebView.loadUrl(hit?.url.orEmpty())
+        myWebView.apply {
+            loadUrl(hit?.url.orEmpty())
+            webViewSetting()
+        }
         supportActionBar?.title = hit?.title
+    }
+
+    fun webViewSetting() {
+        var webSetting = binding.wvContent.settings
+        webSetting.apply {
+            javaScriptEnabled = true
+            domStorageEnabled = true
+        }
+        binding.wvContent.webViewClient = WebViewClient()
     }
 
     companion object {
